3b11a77a949919a37e3c63d7430e9cc442ddc189
Usama Basharat <dev.usamabasharat@gmail.com>
2024-10-09 07:32:05 -0700

    Implemented create issues APIs and UI and error handling

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Callout, TextArea, TextField } from "@radix-ui/themes";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+
+type IssueForm = z.infer<typeof createIssueSchema>;
+
+const NewIssuePage = () => {
+  const [error, setError] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const {
+    control,
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
+  const router = useRouter();
+
+  const onSubmit = async (data: IssueForm) => {
+    try {
+      setIsSubmitting(true);
+      const response = await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setIsSubmitting(false);
+      setError("An unexpected error occured!");
+    }
+  };
+
+  return (
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+
+      <form className=" space-y-3" onSubmit={handleSubmit(onSubmit)}>
+        <TextField.Root placeholder="Title" {...register("title")} />
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <TextArea resize="vertical" placeholder="Description" {...field} />
+          )}
+        />
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        <Button disabled={isSubmitting}>
+          Submit New Issue {isSubmitting && <Spinner />}
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default NewIssuePage;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@radix-ui/themes'
+import Link from 'next/link'
 import React from 'react'
 
 const IssuesPage = () => {
   return (
-    <div>IssuesPage</div>
+    <div><Button><Link href="/issues/new">New Issue</Link></Button></div>
   )
 }
 
-export default IssuesPage
+export default IssuesPage;
